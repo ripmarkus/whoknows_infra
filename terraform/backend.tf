b45466ab8ac67c5e8e149defce204933235d4ab3
shuresm57 <vsorensen31@gmail.com>
2026-04-24 15:18:53 +0200

    switch remote state backend to Terraform Cloud

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,15 +1,8 @@
 terraform {
-  backend "s3" {
-    endpoints = {
-      s3 = "https://fsn1.your-objectstorage.com"
+  cloud {
+    organization = "your-org"
+    workspaces {
+      name = "whoknows-infra"
     }
-    bucket = "whoknows-tfstate"
-    key    = "whoknows_infra/terraform.tfstate"
-    region = "fsn1"
-
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    skip_region_validation      = true
-    force_path_style            = true
   }
 }
